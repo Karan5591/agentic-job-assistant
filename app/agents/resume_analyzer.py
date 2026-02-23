8be7a1efc31101cfcb3e1185c581b64ddefadc61
Karan Singh <your-email@gmail.com>
2026-02-23 21:33:04 +0530

    Added new updates

--- app/agents/resume_analyzer.py
+++ app/agents/resume_analyzer.py
@@ -1,9 +1,6 @@
-from typing import Dict, Any
 from langchain_openai import ChatOpenAI
-from langchain_core.prompts import ChatPromptTemplate
 from app.rag.query import query_knowledge_base
 import json
-import os
 from dotenv import load_dotenv
 from app.agents.resume_parser import resume_parser
 from app.prompts.resume_prompt import resume_prompt
@@ -11,7 +8,7 @@ from app.prompts.resume_prompt import resume_prompt
 
 load_dotenv()
 
-def analyze_resume(query: str = "Analyze my resume") -> Dict[str, Any]:
+def analyze_resume(query: str):
     """
     Resume Analyzer Agent
     - Retrieves resume content using RAG
@@ -21,29 +18,19 @@ def analyze_resume(query: str = "Analyze my resume") -> Dict[str, Any]:
     # Retrieve resume documents
     docs = query_knowledge_base(
         query=query,
-        
-        k=5
-    )
+        k=5)
 
     if not docs:
         return {"error": "No resume data found"}
 
-    resume_context = "\n\n".join(
-        d["content"] for d in docs
-    )
+    resume_context = " ".join(doc["content"] for doc in docs)
 
-    llm = ChatOpenAI(
-        model="gpt-4o-mini",
-        temperature=0
-    )
+    llm = ChatOpenAI(model="gpt-4o-mini")
     chain = resume_prompt | llm | resume_parser
-    response = chain.invoke(
-        {"resume_text": resume_context})
+    response = chain.invoke({"resume_text": resume_context})
 
     # Parse the JSON
     try:
         return response
     except json.JSONDecodeError:
-        return {
-            "error": "Invalid JSON returned by LLM",
-            "raw_output": response.content}
+        return {"error": "Invalid JSON returned by LLM", "raw_output": response.content}
